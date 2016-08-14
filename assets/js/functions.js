98becab95710c1831dd85a4f73fdb9c99a94e8dc
zacharyrnewton <zacharyrnewton@gmail.com>
2016-08-14 16:34:18 -0400

    changed blog to sl2, changed page smoothscroll func, changed instagram embed

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -109,19 +109,20 @@ function mobileNav() {
 }
 
 //Smooth Scroll
-function smoothScroll (duration) {
-	$('a[href^="#"]').on('click', function(event) {
-
-	    var target = $( $(this).attr('href') );
-
-	    if( target.length ) {
-	        event.preventDefault();
-	        $('html, body').animate({
-	            scrollTop: target.offset().top -89
-	        }, duration);
-	    }
-	});
-}
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top -89
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
 
 //Navigation Shrink
 function navShrink() {
@@ -141,6 +142,5 @@ $(window).scroll(function(){
 
 $(function() {
   mobileNav();
-  smoothScroll();
   navShrink();
 });
